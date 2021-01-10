4119c2ede3d88eb62119fb31867e5e015cc8fab3
Alex Hughes <ahughes1988@gmail.com>
2021-01-10 12:26:09 +0100

    uodate sorting

--- hidden-attributes.js
+++ hidden-attributes.js
@@ -15,6 +15,7 @@ function updatePersonalityTypes() {
     };
     const checkbox = document.getElementById("is_newgen");
     const select = document.getElementById("personality_types");
+    const selected = select.value;
     if (checkbox.checked) {
         for (let key in newGenTypes) {
             // eslint-disable-next-line no-prototype-builtins
@@ -37,7 +38,11 @@ function updatePersonalityTypes() {
             }
         }
     }
-    sortSelect("en", select)
+    if (!select.options.hasOwnProperty(selected)) {
+        select.value = select.options.item(0);
+    }
+    sortSelect("en", select);
+    
 }
 
 function sortSelect(lang, selectNode) {
@@ -207,7 +212,7 @@ function generateSelectorElement(name) {
         });
 
         const newgenSelector = document.getElementById("is_newgen")
-        newgenSelector.addEventListener("click", () => {
+        newgenSelector.addEventListener("change", () => {
             updateTable();
             updatePersonalityTypes();
         });
